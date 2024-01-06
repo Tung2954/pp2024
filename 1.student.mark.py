20b3e7f3fb9e44f5ff6402cda51f3087b1f60095
tungpho2004 <ndtung2004@gmail.com>
2024-01-07 01:56:29 +0700

    update practical 1(3)

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,54 +1,94 @@
 students = []
 courses = []
 
+def check_student_number(students):
+    if len(students) == 0:
+        print("There is no student yet!")
+        return False
+    else:
+        return True
+
+def check_course_number(courses):
+    if len(courses) == 0:
+        print("There is no course yet!")
+        return False
+    else:
+        return True
+    
 def number_of_students():
     return int(input("Enter the number of student(s): "))
 
+def check_unique_id_student(student_id):
+    return all(student['info']['id'] != student_id for student in students)
+
 def info_students():
-    student_id = input("Enter student id: ")
-    student_name = input("Enter student name: ")
-    student_dob = input("Enter student dob: ")
-    return {"id": student_id, "name": student_name, "dob": student_dob, "courses": []}
+    while True:
+        student_id = input("Enter student id: ")
+        if check_unique_id_student(student_id):
+            break
+        else:
+            print("Already have ID. Enter another one.")
+        student_name = input("Enter student name: ")
+        student_dob = input("Enter student dob: ")
+        return {"id": student_id, "name": student_name, "dob": student_dob, "courses": []}
 
 def number_of_courses():
     return int(input("Enter the number of course(s): "))
 
+def check_unique_id_course(course_id):
+    return all(course['info']['id'] != course_id for course in courses)
+
 def info_courses():
-    course_id = input("Enter course id: ")
-    course_name = input("Enter course name: ")
-    return {"id": course_id, "name": course_name}
+    while True:
+        course_id = input("Enter course id: ")
+        if check_unique_id_course(course_id):
+            break
+        else:
+            print("Already have ID. Enter another one.")
+        course_name = input("Enter course name: ")
+        return {"id": course_id, "name": course_name}
 
-def input_marks(course, student):
-    marks = float(input(f"Enter the mark for {course['name']} of the student {student['info']['name']}: "))
-    student['info']['courses'].append({"id": course['id'], "marks": marks})
+def input_marks(course, student, students, courses):
+    if not check_student_number(students) and not check_course_number(courses):
+        print("There is no student and no course yet!")
+    elif not check_student_number(students):
+        print("There is no student yet!")
+    elif not check_course_number(courses):
+        print("There is no course yet!")
+    else:
+        marks = float(input(f"Enter the mark for {course['name']} of the student {student['info']['name']}: "))
+        student['info']['courses'].append({"id": course['id'], "marks": marks})
 
-def select_course():
-    print("Select a course")
-    list_courses()
-    course_id = input("Enter the course ID: ")
+def select_course(courses):
+    if check_course_number(courses):
+        print("Select a course")
+        list_courses()
+        course_id = input("Enter the course ID: ")
     
-    for course in courses:
-        if course['id'] == course_id:
-            return course
+        for course in courses:
+            if course['id'] == course_id:
+                return course
 
-def list_students():
-    print("List of students: ")
-    for student in students:
-        print(f"Student ID: {student['info']['id']} - Name: {student['info']['name']}")
+def list_students(students):
+    if check_student_number(students):
+        print("List of students: ")
+        for student in students:
+            print(f"Student ID: {student['info']['id']} - Name: {student['info']['name']}")
 
-def list_courses():
-    print("List of courses: ")
-    for course in courses:
-        print(f"Course ID: {course['id']} - Name: {course['name']}")    
+def list_courses(courses):
+    if check_course_number(courses):
+        print("List of courses: ")
+        for course in courses:
+            print(f"Course ID: {course['id']} - Name: {course['name']}")    
         
-def show_student_marks(student, course):
-    student_id = student['info']['id']
-    course_id = course['id']
+def show_student_marks(student, course, students, courses):
+        student_id = student['info']['id']
+        course_id = course['id']
     
-    for student_course in student['info']['courses']:
-        if student_course['id'] == course_id:
-            print(f"Mark for student name {student['info']['name']} in course {course['name']}: {student_course['marks']}")
-            return
+        for student_course in student['info']['courses']:
+            if student_course['id'] == course_id:
+                print(f"Mark for student name {student['info']['name']} in course {course['name']}: {student_course['marks']}")
+                return
 
 def list_of_choice():
     print(" ---------------------------------------")
@@ -77,37 +117,28 @@ def select_choice():
                 courses.append(course_info)
             
         elif choice == 3:
-            selected_course = select_course()
+            selected_course = select_course(courses)
             if selected_course:
                 for student in students:
                     input_marks(selected_course, student)
             
         elif choice == 4:
-            list_students()
+            list_students(students)
         
         elif choice == 5:
-            list_courses()
+            list_courses(courses)
     
         elif choice == 6:
-            selected_student_id = input("Enter the student ID to show marks: ")
-            selected_student = next((student for student in students if student['info']['id'] == selected_student_id), None)
+            if check_student_number(students) and check_course_number(courses):
+                selected_student_id = input("Enter the student ID to show marks: ")
+                selected_student = next((student for student in students if student['info']['id'] == selected_student_id), None)
 
-            if selected_student:
-                show_student_marks(selected_student, selected_course)
-            else:
-                print(f"No student found with ID {selected_student_id}")
+                if selected_student:
+                    show_student_marks(selected_student, selected_course)
+                else:
+                    print(f"No student found with ID {selected_student_id}")
             
         elif choice == 7:
             return 0
         
-num_student = number_of_students()
-for _ in range(num_student):
-    student_info = info_students()
-    students.append({"info": student_info, "marks": {}})
-
-num_course = number_of_courses()
-for _ in range(num_course):
-    course_info = info_courses()
-    courses.append(course_info)
-    
 select_choice()
